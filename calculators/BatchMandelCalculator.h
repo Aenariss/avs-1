1123beea328e9d52f0e29cd8ccd98f3dd87b33bd
aenariss <aenariss@gmail.com>
2022-11-20 22:12:21 +0100

    finished

--- calculators/BatchMandelCalculator.h
+++ calculators/BatchMandelCalculator.h
@@ -2,7 +2,7 @@
  * @file BatchMandelCalculator.h
  * @author Vojtech Fiala <xfiala61@stud.fit.vutbr.cz>
  * @brief Implementation of Mandelbrot calculator that uses SIMD paralelization over small batches
- * @date DATE
+ * @date 18.11.2022
  */
 #ifndef BATCHMANDELCALCULATOR_H
 #define BATCHMANDELCALCULATOR_H
